9f8f119efe3f22ba0efef1545a10761a65490079
Anton Lilja <antlil@omegapoint.se>
2021-10-08 08:44:42 +0200

    adds frontend in react

---
+++ react/contracts/GuessMyNumber.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+contract GuessMyNumber {
+    address public owner;
+    uint16 secretNumber;
+
+    event guessEvent(uint16 guess, string message);
+
+    constructor() {
+       owner = msg.sender;
+    }
+
+    function guess (uint16 guess_number) public {
+        //TODO implement this!
+        emit guessEvent(guess_number, "This was your guess");
+    }
+
+    function setSecretNumber (uint16 newSecret) public {
+      require(msg.sender == owner);
+      secretNumber = newSecret;
+    }
+
+}
