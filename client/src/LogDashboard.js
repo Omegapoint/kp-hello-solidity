95108bd1f37b1e47a352d90c0e3eeabcf1c0b992
Anton Lilja <antlil@omegapoint.se>
2021-10-08 12:20:09 +0200

    working log

--- client/src/LogDashboard.js
+++ client/src/LogDashboard.js
@@ -4,12 +4,13 @@ import React from 'react';
 class LogDashboard extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: ['']};
+    this.state = {value: ['123123']};
     this.event = props.event;
     this.contract = props.contract;
   }
 
-  componentWillMount = () => {
+  componentDidMount = () => {
+    const component = this
     this.contract.events.allEvents("AllEvents",{  
       fromBlock: 'latest',
       }, function (error, event) {
@@ -18,17 +19,24 @@ class LogDashboard extends React.Component {
             console.log(error)
           }
           console.log(event.returnValues);
-          }
+          component.setState(prevState => {
+            console.log(prevState)
+            return { value: [...prevState.value, event.returnValues[0] + " - " + event.returnValues[1]] }
+          })
+        }
       );
   }
 
   render() {
     return (
-      <li key={this.state.value}>
-        {this.state.value}
-      </li>
+      this.state.value.map( v=> {
+      return (<li key={v}>
+        {v}
+      </li>)
+      }
+      )
     );
-  }
+    }
 }
 
 export default LogDashboard;
